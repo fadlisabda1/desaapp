d3fc47de32bc345e6042eafb898205aa8271d01e
fadlisabda <fadlisabdad@yahoo.com>
2022-10-06 21:24:29 +0700

    menambahkan profil desa next perbaiki code dan tambah profil lain

--- app/Views/profilDesa/index.php
+++ app/Views/profilDesa/index.php
@@ -6,18 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Profil | Desa Tanah Merah</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <style>
-        body {
-            height: 1000px;
-        }
-    </style>
+    <!-- MY CSS -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-success shadow-sm fixed-top">
         <div class="container">
-            <a class="navbar-brand page-scroll" href="#tes">Desa Tanah Merah</a>
+            <a class="navbar-brand page-scroll" href="#tes"><img src="gambar/LogoKampar.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"><span class="text-wrap badge bg-success" style="width:14rem">Desa Tanah Merah Kabupaten Kampar</span></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -40,7 +37,134 @@
         </div>
     </nav>
     <!-- Akhir Navbar -->
-    <p id="Home" style="margin-top: 1000px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores reprehenderit distinctio deserunt cum placeat labore officiis, provident culpa consectetur et, excepturi necessitatibus, rem vero fugit voluptas. Ipsum ut, quos laborum blanditiis facilis molestiae quam illo, corporis nulla alias tenetur cum dicta voluptatibus nesciunt perspiciatis asperiores possimus recusandae facere. Tempore id inventore eveniet reiciendis. Blanditiis mollitia minima praesentium provident, sed libero sint facere nemo iusto magni quae repudiandae eligendi, porro, at aperiam commodi? Dignissimos odit facere officia consequatur praesentium? Ratione nisi, maxime consectetur ipsam ipsa accusamus doloremque exercitationem magnam cupiditate. Deserunt dolores aperiam odio amet inventore perspiciatis voluptate eaque aliquid? Accusantium.</p>
+    <!-- Jumbotron -->
+    <section class="jumbotron mt-5 text-center">
+        <h1 class="display-4" style="color: white;">Selamat Datang</h1>
+        <p class="lead" style="color: white;">Di Website Desa Tanah Merah</p>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#fff" fill-opacity="1" d="M0,64L30,101.3C60,139,120,213,180,240C240,267,300,245,360,208C420,171,480,117,540,128C600,139,660,213,720,229.3C780,245,840,203,900,154.7C960,107,1020,53,1080,37.3C1140,21,1200,43,1260,64C1320,85,1380,107,1410,117.3L1440,128L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path>
+        </svg>
+    </section>
+    <!-- Akhir Jumbotron -->
+    <!-- Profil Desa -->
+    <section id="profil">
+        <div class="container">
+            <div class="row text-center mb-3">
+                <div class="col">
+                    <h2 style="color: green;">Profil Desa</h2>
+                </div>
+            </div>
+            <div class="row row-cols-1 row-cols-md-2 g-4 text-left justify-content-center">
+                <div class="col">
+                    <div class="card text-bg-success">
+                        <img src="gambar/desadepan.jpeg" class="card-img-top" alt="desadepan">
+                        <div class="card-body">
+                            <h5 class="card-title">Sejarah Desa Tanah Merah</h5>
+                            <p class="card-text">Desa Tanah Merah adalah nama suatu wilayah di Kecamatan Siak Hulu
+                                Kabupaten Kampar ini yang nama Tanah Merah tersebut diambil dari salah satu nama
+                                RK (Rukun Kampung) yang saat ini menjadi salah satu wilayah dusun berbatas
+                                lansung dengan Desa Baru sebagai Desa Induk. Pemekeran Desa Tanah Merah adalah
+                                usulan dari para tokoh masyarakat setempat pada tahun 1999, berdasarkan Surat
+                                Keputusan Kepala Daerah Tingkat 1 Riau Nomor 41 Tahun 1999 tentang Pemekaran
+                                Desa persiapan Tanah Merah maka Desa Tanah Merah secara resmi menjadi
+                                Pemerintahan Desa persiapan. Desa Tanah Merah “sebelah Utara berbatas dengan
+                                kelurahan Simpang Tiga Kota Pekanbaru, sebelah Timur berbatas dengan Desa Baru,
+                                sebelah Selatan berbatas dengan Desa Pandau Jaya dan sebelah Barat berbatas
+                                Dengan Kelurahan Simpang Tiga Kecamatan Bukitraya Kota Pekanbaru. Sedangkan
+                                luas wilayah diperkirakan sekitar 1.096 Ha dan ketinggian tanah dari permukaan laut
+                                sekitar 20 M, banyaknya curah hujan sekitar 2545 MM/HM, topografi (Dataran
+                                Rendah, Tinggi Pantai) sekitar 75,25 MM/HM dan suhu rata — rata 35'C. Adapun
+                                jarak tempuh menuju Ibu Kota Kecamatan diperkirakan sepanjang 10 KM, ke Ibu
+                                Kota Kabupaten sepanjang 70 KM dan ke Ibu Kota Provinsi sepanjang 7,5 KM.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Visi Misi Desa -->
+        <div id="visimisi">
+            <div class="container">
+                <div class="row mt-5">
+                    <div class="col-md-6 mb-3">
+                        <div class="card text-bg-success">
+                            <div class="card-body">
+                                <h2>Visi</h2>
+                                <p class="card-text">Terwujudnya pelayanan public yang transparan, akuntabilitas menuju Masyarakat Desa Tanah Merah yang maju, aman, dan sejahterah.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card text-bg-success">
+                            <div class="card-body">
+                                <h2>Misi</h2>
+                                <ol class="list-group list-group-numbered">
+                                    <li class="list-group-item">
+                                        <p class="card-text">Melanjutkan program-program Pemerintah Desa Tanah Merah periode lalu, sebagai mana tercantum dalam Dokumen RPJMDes Desa Tanah Merah.</p>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <p class="card-text">Pemberdayaan semua potensi yang ada dimasyarakat meliputi :</p>
+                                        <ol class="list-group list-group-numbered">
+                                            <li class="list-group-item list-group-item-success">
+                                                <p class="card-text">Pemberdayaan SDM</p>
+                                            </li>
+                                            <li class="list-group-item list-group-item-success">
+                                                <p class="card-text">Pemberdayaan Ekonomi Kerakyatan</p>
+                                            </li>
+                                        </ol>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <p class="card-text">Menciptakan kondisi masyarakat Desa Tnaah Merah yang aman, tertib dan rukun dalam kehidupan bermasyarakat dengan berpegang pada prinsip-prinsip yaitu :</p>
+                                        <ol class="list-group list-group-numbered">
+                                            <li class="list-group-item list-group-item-success">
+                                                <p class="card-text">Duduk sama rendah berdiri sama tinggi</p>
+                                            </li>
+                                            <li class="list-group-item list-group-item-success">
+                                                <p class="card-text">Ringan sama dijinjing berat sama dipikul</p>
+                                            </li>
+                                        </ol>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <p class="card-text">Optimalisasi penyelenggaraan pemerintahan Desa Tanah Merah yang meliputi :</p>
+                                        <ol class="list-group list-group-numbered">
+                                            <li class="list-group-item list-group-item-success">
+                                                <p class="card-text">Penyelenggaraan pemerintahan yang transparan dan akuntabel</p>
+                                            </li>
+                                            <li class="list-group-item list-group-item list-group-item-success">
+                                                <p class="card-text">Pelayanan kepada masyrakat yang Prima yaitu cepat, tepat dan benar.</p>
+                                            </li>
+                                            <li class="list-group-item list-group-item list-group-item-success">
+                                                <p class="card-text">Pelaksanaan pembangunan yang berkesinambungan dan mengedepankan partisipasi dan gotong royong masyarakat.</p>
+                                            </li>
+                                        </ol>
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Akhir Visi Misi Desa -->
+        <!-- Peta Wilayah Desa -->
+        <div id="petaWilayahDesa">
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col">
+                        <h2>Peta Wilayah Desa</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container text-center">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1994.851475493056!2d101.45914275863554!3d0.4357198573712268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x292cb83986560007!2sKantor%20Kades%20Tanah%20Merah!5e0!3m2!1sid!2sid!4v1665065294819!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <!-- Akhir Peta Wilayah Desa -->
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="rgb(25,135,84)" fill-opacity="1" d="M0,64L20,106.7C40,149,80,235,120,240C160,245,200,171,240,138.7C280,107,320,117,360,144C400,171,440,213,480,197.3C520,181,560,107,600,74.7C640,43,680,53,720,80C760,107,800,149,840,176C880,203,920,213,960,218.7C1000,224,1040,224,1080,202.7C1120,181,1160,139,1200,122.7C1240,107,1280,117,1320,144C1360,171,1400,213,1420,234.7L1440,256L1440,320L1420,320C1400,320,1360,320,1320,320C1280,320,1240,320,1200,320C1160,320,1120,320,1080,320C1040,320,1000,320,960,320C920,320,880,320,840,320C800,320,760,320,720,320C680,320,640,320,600,320C560,320,520,320,480,320C440,320,400,320,360,320C320,320,280,320,240,320C200,320,160,320,120,320C80,320,40,320,20,320L0,320Z"></path>
+        </svg>
+    </section>
+    <!-- Akhir Profil Desa -->
+
     <script src="js/jquery-3.1.1.js"></script>
     <script src="js/jquery.easing.1.3.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
